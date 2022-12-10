fb02a812b6db7bb74f32a20b24fd75208b9478f9
chudi ukaga <chudiukaga08@gmail.com>
2022-12-10 01:31:47 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
+
 /**
- * main-Entry
- * description: i am writing my first c programme
+ * main - Entry
+ *
  * Return: Always 0
  *
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multi lingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
