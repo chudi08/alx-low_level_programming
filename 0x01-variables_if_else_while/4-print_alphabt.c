c852b75e0b2a8e3b54a69431f98955fa4ee37590
chudi ukaga <chudiukaga08@gmail.com>
2022-12-11 00:57:07 -0800

    When i was having that alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - This code prints all letters except e and q
+ * followed by a white line
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
