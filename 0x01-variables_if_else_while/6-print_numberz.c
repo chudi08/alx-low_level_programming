d860304c6d6f776ec2d9b08d2078e9b9bfbacbc3
chudi ukaga <chudiukaga08@gmail.com>
2022-12-11 01:42:54 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Followed by new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n = 48;
+
+	while (n < 58)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
