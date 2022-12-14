4752a3bcd2e1b524c906aca974e38276c7618f92
chudi ukaga <chudiukaga08@gmail.com>
2022-12-14 07:56:42 -0800

    There are only 3 colours

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * @n: to print last number
+ * Return: value of the last digit
+ */
+int print_last_digit(int n)
+{
+	int a;
+
+	if (n < 0)
+		n = -n;
+
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
+
+	return (a);
+}
