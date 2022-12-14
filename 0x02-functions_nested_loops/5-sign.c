f4b4c26a464ab26f373726803bd0341f8bf2c262
chudi ukaga <chudiukaga08@gmail.com>
2022-12-14 07:31:06 -0800

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - Entry point
+ * @n: checks integer n
+ * Return: Success
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
