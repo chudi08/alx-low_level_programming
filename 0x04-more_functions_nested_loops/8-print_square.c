00749e015d1ab91120f46d730dee3a316f6902ae
chudi ukaga <chudiukaga08@gmail.com>
2022-12-31 06:07:53 -0800

    Fizz Buzz

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * print-square - prints a square, followed by a new line;
+ * print-square - prints a square
+ * Description: prints the size of a square
  * @size: size of the square
  */
 void print_square(int size)
